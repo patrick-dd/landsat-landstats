0f1cdd5c7d354ae34530dfdc20fa475311b12d67
Ubuntu <ubuntu@ip-172-31-26-13.ec2.internal>
2016-02-07 04:52:50 +0000

    updated loop for memory conservation

--- spatialInterpolation.py
+++ spatialInterpolation.py
@@ -33,8 +33,6 @@ from databaseConstructor import pixelToCoordinates
 
 # return (lon_coord, lat_coord)
 
-
-
 # loading predictions
 y_pred = np.array(cPickle.load(file('output_predictions.save', 'rb')))
 
@@ -49,12 +47,11 @@ X_train = X_train[0:len(y_pred)]
 filename = 'LANDSAT_TOA/Washington/washington_b1.tif'
 satellite_gdal = gdal.Open(filename)
 ncols, nrows = satellite_gdal.RasterXSize, satellite_gdal.RasterYSize
-cols_grid, rows_grid = np.meshgrid(range(0,ncols), range(0,nrows))
 
 # chopping up the grid into overlapping ranges
 # columns
-batch_size = 2000
-overlap = 500
+batch_size = 1500
+overlap = 350
 batch_size_less_olap = batch_size - overlap
 col_regions = ncols / batch_size_less_olap + 1
 col_slice_lower = [batch_size_less_olap * x for x in np.arange(col_regions)]
@@ -67,30 +64,28 @@ row_slice_lower = [batch_size_less_olap * x for x in np.arange(row_regions)]
 row_slice_upper = [batch_size_less_olap * x + batch_size for x in np.arange(row_regions)]
 row_slice_upper[-1] = nrows
 row_length = len(row_slice_upper)
+print 'Makin batches'
+n_neighbors = 5
+knn = neighbors.KNeighborsRegressor(n_neighbors, weights='distance')
+model = knn.fit(X_train, y_pred)
 
-batches = []
+count = 0
 for i in np.arange(len(row_slice_upper)):
 	for j in np.arange(len(col_slice_upper)):
-		tmp_batch_cols = cols_grid[ row_slice_lower[i]:row_slice_upper[i], 
-									col_slice_lower[j]:col_slice_upper[j] ]
-		tmp_batch_rows = rows_grid[ row_slice_lower[i]:row_slice_upper[i], 
-									col_slice_lower[j]:col_slice_upper[j] ]
-		batches.append((tmp_batch_rows, tmp_batch_cols))
+		print count
+		tmp_batch_rows, tmp_batch_cols = np.meshgrid(
+									np.arange(row_slice_lower[i], row_slice_upper[i]), 
+									np.arange(col_slice_lower[j], col_slice_upper[j]))
+		tmp_col_grid = tmp_batch_cols.ravel()
+		tmp_row_grid = tmp_batch_rows.ravel()		
+		tmp_location_series = [Point(pixelToCoordinates(
+								satellite_gdal.GetGeoTransform(), col, row)) \
+								for (col, row) in zip(tmp_col_grid, tmp_row_grid)]
+		tmp_coordinates = [ (point.y, point.x) for point in tmp_location_series]
+		y_interpolated = model.predict(tmp_coordinates)
+		print y_interpolated
+		cPickle.dump(y_interpolated, file('y_interpolated_%d.save' % count, 
+					'wb'), protocol= cPickle.HIGHEST_PROTOCOL)
+		count += 1
 
-batches=np.array(batches)
-no_batches = batches.shape[0]
-n_neighbors = 5
-y_interpolated = []
-
-for batch in batches:
-	tmp_row_grid = batch[0].ravel()
-	tmp_col_grid = batch[1].ravel()
-	tmp_location_series = [Point(pixelToCoordinates(satellite_gdal.GetGeoTransform(), col, row)) \
-						for (col, row) in zip(tmp_col_grid, tmp_row_grid)]
-	tmp_coordinates = [ (point.y, point.x) for point in location_series]
-	knn = neighbors.KNeighborsRegressor(n_neighbors, weights='distance')
-	y_interpolated.append(knn.fit(X_train, y_pred).predict(tmp_coordinates))
-
-cPickle.dump(y_interpolated, file('y_interpolated.save', 'wb'), protocol=
-			cPickle.HIGHEST_PROTOCOL)
 
